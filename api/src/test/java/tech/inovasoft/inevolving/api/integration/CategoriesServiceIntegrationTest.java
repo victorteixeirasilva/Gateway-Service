3c9435b41cf9aee14b8a6bc938a4830097b68355
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-06-18 17:00:52 -0300

    Desenvolvendo coste de integração do categories-service

---
+++ api/src/test/java/tech/inovasoft/inevolving/api/integration/CategoriesServiceIntegrationTest.java
@@ -0,0 +1,32 @@
+package tech.inovasoft.inevolving.api.integration;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.ValidatableResponse;
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class CategoriesServiceIntegrationTest {
+
+    @Test
+    public void integrationTest_Ok() {
+
+        RequestSpecification requestSpecification = given()
+                .contentType(ContentType.JSON);
+
+        ValidatableResponse response = requestSpecification
+                .when()
+                .get("http://localhost:8081/actuator/health")
+                .then();
+
+        response.assertThat().statusCode(200).and()
+                .body("status", equalTo("UP"));
+
+    }
+
+}
