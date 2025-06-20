7de0e9582074cd82cb07e7a33ba1a76a23be1adb
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-06-20 19:07:26 -0300

    Desenvolvendo métodos do CategoriesService

--- api/src/main/java/tech/inovasoft/inevolving/api/service/CategoriesService.java
+++ api/src/main/java/tech/inovasoft/inevolving/api/service/CategoriesService.java
@@ -1,6 +1,13 @@
 package tech.inovasoft.inevolving.api.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import tech.inovasoft.inevolving.api.domain.exception.BooksServiceException;
+import tech.inovasoft.inevolving.api.domain.exception.CategoriesServiceException;
+import tech.inovasoft.inevolving.api.service.client.books_service.dto.Book;
+import tech.inovasoft.inevolving.api.service.client.categories_service.CategoriesServiceClient;
 import tech.inovasoft.inevolving.api.service.client.categories_service.dto.*;
 
 import java.util.UUID;
@@ -8,44 +15,224 @@ import java.util.UUID;
 @Service
 public class CategoriesService {
 
+    @Autowired
+    private CategoriesServiceClient categoriesServiceClient;
+
     public Category addCategory(
             UUID idUser,
             RequestCategoryDTO dto
     ) {
-        //TODO: implement
-        return null;
+        ResponseEntity<Category> response = categoriesServiceClient.addCategory(idUser, dto);
+
+        if (response.getStatusCode().isSameCodeAs(HttpStatus.INTERNAL_SERVER_ERROR)) {
+            throw new CategoriesServiceException(
+                    "Error (INTERNAL_SERVER_ERROR) in micro service (categories-service), during operation (categoriesServiceClient.addCategory(idUser, dto)).",
+                    HttpStatus.INTERNAL_SERVER_ERROR
+            );
+        }
+
+        if (response.getStatusCode().isSameCodeAs(HttpStatus.FORBIDDEN)) {
+            throw new BooksServiceException(
+                    "Error (FORBIDDEN) in micro service (categories-service), during operation (categoriesServiceClient.addCategory(idUser, dto)).",
+                    HttpStatus.FORBIDDEN
+            );
+        }
+
+        if (response.getStatusCode().isSameCodeAs(HttpStatus.NOT_FOUND)) {
+            throw new BooksServiceException(
+                    "Error (NOT_FOUND) in micro service (categories-service), during operation (categoriesServiceClient.addCategory(idUser, dto)).",
+                    HttpStatus.NOT_FOUND
+            );
+        }
+
+        return response.getBody();
     }
 
     public ResponseCategoryAndNewObjectiveDTO addObjectiveToCategory(
             UUID idUser,
             RequestAddObjectiveToCategoryDTO dto
     ) {
-        //TODO: implement
-        return null;
+        ResponseEntity<ResponseCategoryAndNewObjectiveDTO> response =
+                categoriesServiceClient.addObjectiveToCategory(idUser, dto);
+
+        if (response.getStatusCode().isSameCodeAs(HttpStatus.INTERNAL_SERVER_ERROR)) {
+            throw new CategoriesServiceException(
+                    "Error (INTERNAL_SERVER_ERROR) in micro service (categories-service), during operation (categoriesServiceClient.addObjectiveToCategory).",
+                    HttpStatus.INTERNAL_SERVER_ERROR
+            );
+        }
+
+        if (response.getStatusCode().isSameCodeAs(HttpStatus.FORBIDDEN)) {
+            throw new BooksServiceException(
+                    "Error (FORBIDDEN) in micro service (categories-service), during operation (categoriesServiceClient.addObjectiveToCategory).",
+                    HttpStatus.FORBIDDEN
+            );
+        }
+
+        if (response.getStatusCode().isSameCodeAs(HttpStatus.NOT_FOUND)) {
+            throw new BooksServiceException(
+                    "Error (NOT_FOUND) in micro service (categories-service), during operation (categoriesServiceClient.addObjectiveToCategory).",
+                    HttpStatus.NOT_FOUND
+            );
+        }
+
+        return response.getBody();
     }
 
-    public ResponseMessageDTO removeObjectiveToCategory(UUID idUser, UUID idCategory, UUID idObjective) {
-        //TODO: implement
-        return null;
+    public ResponseMessageDTO removeObjectiveToCategory(
+            UUID idUser,
+            UUID idCategory,
+            UUID idObjective
+    ) {
+        ResponseEntity<ResponseMessageDTO> response =
+                categoriesServiceClient.removeObjectiveToCategory(idUser, idCategory, idObjective);
+
+        if (response.getStatusCode().isSameCodeAs(HttpStatus.INTERNAL_SERVER_ERROR)) {
+            throw new CategoriesServiceException(
+                    "Error (INTERNAL_SERVER_ERROR) in micro service (categories-service), during operation (categoriesServiceClient.removeObjectiveToCategory).",
+                    HttpStatus.INTERNAL_SERVER_ERROR
+            );
+        }
+
+        if (response.getStatusCode().isSameCodeAs(HttpStatus.FORBIDDEN)) {
+            throw new BooksServiceException(
+                    "Error (FORBIDDEN) in micro service (categories-service), during operation (categoriesServiceClient.removeObjectiveToCategory).",
+                    HttpStatus.FORBIDDEN
+            );
+        }
+
+        if (response.getStatusCode().isSameCodeAs(HttpStatus.NOT_FOUND)) {
+            throw new BooksServiceException(
+                    "Error (NOT_FOUND) in micro service (categories-service), during operation (categoriesServiceClient.removeObjectiveToCategory).",
+                    HttpStatus.NOT_FOUND
+            );
+        }
+
+        return response.getBody();
     }
 
-    public ResponseMessageDTO removeCategory(UUID idUser, UUID idCategory) {
-        //TODO: implement
-        return null;
+    public ResponseMessageDTO removeCategory(
+            UUID idUser,
+            UUID idCategory
+    )
+    {
+        ResponseEntity<ResponseMessageDTO> response =
+                categoriesServiceClient.removeCategory(idUser, idCategory);
+
+        if (response.getStatusCode().isSameCodeAs(HttpStatus.INTERNAL_SERVER_ERROR)) {
+            throw new CategoriesServiceException(
+                    "Error (INTERNAL_SERVER_ERROR) in micro service (categories-service), during operation (categoriesServiceClient.removeCategory).",
+                    HttpStatus.INTERNAL_SERVER_ERROR
+            );
+        }
+
+        if (response.getStatusCode().isSameCodeAs(HttpStatus.FORBIDDEN)) {
+            throw new BooksServiceException(
+                    "Error (FORBIDDEN) in micro service (categories-service), during operation (categoriesServiceClient.removeCategory).",
+                    HttpStatus.FORBIDDEN
+            );
+        }
+
+        if (response.getStatusCode().isSameCodeAs(HttpStatus.NOT_FOUND)) {
+            throw new BooksServiceException(
+                    "Error (NOT_FOUND) in micro service (categories-service), during operation (categoriesServiceClient.removeCategory).",
+                    HttpStatus.NOT_FOUND
+            );
+        }
+
+        return response.getBody();
     }
 
-    public Category updateCategory(UUID idUser, UUID idCategory, RequestUpdateCategoryDTO dto) {
-        //TODO: implement
-        return null;
+    public Category updateCategory(
+            UUID idUser,
+            UUID idCategory,
+            RequestUpdateCategoryDTO dto
+    ) {
+        ResponseEntity<Category> response =
+                categoriesServiceClient.updateCategory(idUser, idCategory, dto);
+
+        if (response.getStatusCode().isSameCodeAs(HttpStatus.INTERNAL_SERVER_ERROR)) {
+            throw new CategoriesServiceException(
+                    "Error (INTERNAL_SERVER_ERROR) in micro service (categories-service), during operation (categoriesServiceClient.updateCategory).",
+                    HttpStatus.INTERNAL_SERVER_ERROR
+            );
+        }
+
+        if (response.getStatusCode().isSameCodeAs(HttpStatus.FORBIDDEN)) {
+            throw new BooksServiceException(
+                    "Error (FORBIDDEN) in micro service (categories-service), during operation (categoriesServiceClient.updateCategory).",
+                    HttpStatus.FORBIDDEN
+            );
+        }
+
+        if (response.getStatusCode().isSameCodeAs(HttpStatus.NOT_FOUND)) {
+            throw new BooksServiceException(
+                    "Error (NOT_FOUND) in micro service (categories-service), during operation (categoriesServiceClient.updateCategory).",
+                    HttpStatus.NOT_FOUND
+            );
+        }
+
+        return response.getBody();
     }
 
-    public ResponseCategoriesDTO getCategories(UUID idUser) {
-        //TODO: implement
-        return null;
+    public ResponseCategoriesDTO getCategories(
+            UUID idUser
+    ) {
+        ResponseEntity<ResponseCategoriesDTO> response =
+                categoriesServiceClient.getCategories(idUser);
+
+        if (response.getStatusCode().isSameCodeAs(HttpStatus.INTERNAL_SERVER_ERROR)) {
+            throw new CategoriesServiceException(
+                    "Error (INTERNAL_SERVER_ERROR) in micro service (categories-service), during operation (categoriesServiceClient.getCategories).",
+                    HttpStatus.INTERNAL_SERVER_ERROR
+            );
+        }
+
+        if (response.getStatusCode().isSameCodeAs(HttpStatus.FORBIDDEN)) {
+            throw new BooksServiceException(
+                    "Error (FORBIDDEN) in micro service (categories-service), during operation (categoriesServiceClient.getCategories).",
+                    HttpStatus.FORBIDDEN
+            );
+        }
+
+        if (response.getStatusCode().isSameCodeAs(HttpStatus.NOT_FOUND)) {
+            throw new BooksServiceException(
+                    "Error (NOT_FOUND) in micro service (categories-service), during operation (categoriesServiceClient.getCategories).",
+                    HttpStatus.NOT_FOUND
+            );
+        }
+
+        return response.getBody();
     }
 
-    public ResponseObjectivesByCategory getObjectivesByCategory(UUID idUser, UUID idCategory) {
-        //TODO: implement
-        return null;
+    public ResponseObjectivesByCategory getObjectivesByCategory(
+            UUID idUser,
+            UUID idCategory
+    ) {
+        ResponseEntity<ResponseObjectivesByCategory> response =
+                categoriesServiceClient.getObjectivesByCategory(idUser, idCategory);
+
+        if (response.getStatusCode().isSameCodeAs(HttpStatus.INTERNAL_SERVER_ERROR)) {
+            throw new CategoriesServiceException(
+                    "Error (INTERNAL_SERVER_ERROR) in micro service (categories-service), during operation (categoriesServiceClient.getObjectivesByCategory).",
+                    HttpStatus.INTERNAL_SERVER_ERROR
+            );
+        }
+
+        if (response.getStatusCode().isSameCodeAs(HttpStatus.FORBIDDEN)) {
+            throw new BooksServiceException(
+                    "Error (FORBIDDEN) in micro service (categories-service), during operation (categoriesServiceClient.getObjectivesByCategory).",
+                    HttpStatus.FORBIDDEN
+            );
+        }
+
+        if (response.getStatusCode().isSameCodeAs(HttpStatus.NOT_FOUND)) {
+            throw new BooksServiceException(
+                    "Error (NOT_FOUND) in micro service (categories-service), during operation (categoriesServiceClient.getObjectivesByCategory).",
+                    HttpStatus.NOT_FOUND
+            );
+        }
+
+        return response.getBody();
     }
 }
